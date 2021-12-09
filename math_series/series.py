42e4e34afd50b840c1efab2702659c08ae5ff7b2
Ediberto Ponce <ediberto01@gmail.com>
2021-12-08 21:56:31 -0800

    sum_series function passes one of two tests

--- math_series/series.py
+++ math_series/series.py
@@ -1,8 +1,10 @@
 def fibonacci(n):
   """
   rule out invalid numbers, anything < 0.
+  BASE CASE:
   if n = 0 then return 0
   if n = 1 or 2 then return 1
+  ----------------------------------------------------------------
   if n greater than 2, then return function(n-1) + function(n-2)
   check test for code results
   """
@@ -16,8 +18,32 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 
 
-# def lucas(n):
-#   """
-#   sdf
-#   """
-#   pass
+def lucas(n):
+  """
+  similar to above but adjust the base case
+  if n = 0 then return 2
+  if n = 1 then return 1
+  """
+  if n == 0:
+      return 2
+  if n == 1:
+    return 1
+  else:
+    return lucas(n-1) + lucas(n-2)
+
+
+def sum_series(n, a=0, b=1):
+  """
+  similar to above but adjust the base case
+  if n = 0 then return 2
+  if n = 1 then return 1
+  """
+  if n == 0:
+    return a
+  if n == 1:
+    return b
+  if n == 2:
+    return b
+  elif n > 2:
+    return sum_series(n-1, a, b) + sum_series(n-2, a, b)
+    
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,4 @@
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas, sum_series
 
 def test_fibonacci_9():
     expected = 34
@@ -8,4 +8,19 @@ def test_fibonacci_9():
 def test_fibonacci_15():
     expected = 610
     actual = fibonacci(15)
+    assert expected == actual
+
+def test_lucas_10():
+    expected = 123
+    actual = lucas(10)
+    assert expected == actual
+
+def test_sum_series_fibonacci():
+    expected = 13
+    actual = sum_series(7, 0, 1)
+    assert expected == actual
+
+def test_sum_series_lucas():
+    expected = 123
+    actual = sum_series(10, 2, 1)
     assert expected == actual
